a17e31a9e9275c121dae32bf18e3fac5ffaa30f5
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2021-08-30 01:02:39 +0300

    complete toggle functionality

--- app/src/main/java/com/cobanogluhasan/androidcustomsidemenu/MainActivity.kt
+++ app/src/main/java/com/cobanogluhasan/androidcustomsidemenu/MainActivity.kt
@@ -2,10 +2,45 @@ package com.cobanogluhasan.androidcustomsidemenu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
+import com.cobanogluhasan.androidcustomsidemenu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var isMenuShowing = false
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.toggleButton.setOnClickListener { toggleBtn() }
+        //hide the menu on anywhere click
+        binding.container.setOnClickListener { hideMenu() }
+    }
+
+    private fun toggleBtn() {
+        if (!isMenuShowing) {
+            //show the menu if is not showing
+            binding.fragmentContainerView.visibility = View.VISIBLE
+            //prepare and load animation
+            val animation = AnimationUtils.loadAnimation(this, R.anim.slide_in_left_to_right)
+            binding.fragmentContainerView.startAnimation(animation)
+            // isMenuShowing = assign reverse of the current state.
+            // if it's true it'll be false. if it's false, it will be true
+            isMenuShowing = !isMenuShowing
+        } else hideMenu()
+    }
+
+    private fun hideMenu() {
+        if (isMenuShowing) {
+            //hide menu if it's showing
+            binding.fragmentContainerView.visibility = View.INVISIBLE
+            val animation = AnimationUtils.loadAnimation(this, R.anim.slide_out_right_to_left)
+            binding.fragmentContainerView.startAnimation(animation)
+            isMenuShowing = false
+        }
     }
 }
